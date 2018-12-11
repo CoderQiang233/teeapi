638d3155f700227b701a7b442334cf6aaaf1ab8f
niuyuan <337966687@qq.com>
2018-12-11 15:08:39 +0800

    我的地址增删改查接口

--- Weixin/Domain/MyAddresss.php
+++ Weixin/Domain/MyAddresss.php
@@ -11,15 +11,6 @@
 class Domain_MyAddresss
 {
 
-    public function getMyAddresssById($id){
-
-
-        $model = new Model_MyAddresss();
-
-        return $model -> getMyAddresssById($id);
-
-    }
-
     public function insertMyAddresss($data){
 
         try{
@@ -48,31 +39,87 @@ class Domain_MyAddresss
 
     }
 
+    public function  findAddressByMemberId($data){
 
-    public function updateMyAddresss($data){
+        try{
 
-        $model = new Model_MyAddresss();
+            $model=new Model_MyAddresss();
 
-        return $model -> updateMyAddresss($data);
+            $res=$model->findAddressByMemberId($data->member_id);
 
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('查看会员地址信息失败','会员id:'.$data->member_id.'异常信息:'.$e);
+
+            return false;
+        }
     }
 
-    public function getMyAddresssBySession3rd($openid){
+    public function  findAddressById($data){
 
-        $model = new Model_MyAddresss();
+        try{
 
-        return $model ->getMyAddresssBySession3rd($openid);
+            $model=new Model_MyAddresss();
 
+            $res=$model->findAddressById($data->id);
 
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过id查看我的单个地址信息失败','id:'.$data->id.'异常信息:'.$e);
+
+            return false;
+        }
     }
 
-    public function inAddress($openid){
+    public function  updateAddressById($data){
 
-        $model = new Model_MyAddresss();
+        try{
 
-        return $model ->inAddress($openid);
+            $model=new Model_MyAddresss();
 
+            $state=$data->state;
 
+            $addressState=$model->getAddressState($data->member_id);
+
+            if ($state==1&&$addressState['stateTotal']==1){//有默认地址,先将该会员的其他地址信息的state都改为0,再更新
+
+                $model->updateState($data->member_id);
+            }
+
+            $res=$model->updateAddressById($data);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过id修改单个收货信息失败','id:'.$data->id.'异常信息:'.$e);
+
+            return false;
+        }
     }
+
+    public function  deleteAddressById($data){
+
+        try{
+
+            $model=new Model_MyAddresss();
+
+            $res=$model->deleteAddressById($data->id);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过id删除单个地址信息失败','id:'.$data->id.'异常信息:'.$e);
+
+            return false;
+        }
+    }
+
+
 
 }
--- Weixin/Model/MyAddresss.php
+++ Weixin/Model/MyAddresss.php
@@ -11,7 +11,11 @@
 class Model_MyAddresss extends PhalApi_Model_NotORM
 {
 
-
+    /**
+     * @param $data
+     * @return bool
+     * 新增我的地址
+     */
     public function insertMyAddresss($data){
 
         try{
@@ -45,6 +49,67 @@ class Model_MyAddresss extends PhalApi_Model_NotORM
         }
 
     }
+
+    /**
+     * @param $member_id
+     * @return mixed
+     * 查看我的地址,通过会员id查看地址信息
+     */
+    public function findAddressByMemberId($member_id){
+
+        return DI()->notorm->member_address->where('member_id',$member_id)->fetchAll();
+    }
+
+
+    /**
+     * @param $member_id
+     * @return mixed
+     * 通过id查看我的单个地址信息
+     */
+    public function findAddressById($id){
+
+        return DI()->notorm->member_address->where('id',$id)->fetch();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     * 通过id修改单个收货信息
+     */
+    public function updateAddressById($data){
+
+        //通过province省查询出对应的地图编码
+        $province=DI()->notorm->province->where('name',$data->province)->fetchOne();
+
+        $info=array(
+            'address'=>$data->address,
+            'consignee_name'=>$data->consignee_name,
+            'consignee_phone'=>$data->consignee_phone,
+            'city'=>$data->city,
+            'county'=>$data->county,
+            'province'=>$data->province,
+            'map_code'=>$province['map_code'],
+            'state'=>$data->state,
+        );
+
+        $rs=DI()->notorm->member_address->where('id',$data->id)->update($info);
+
+        return $rs;
+    }
+
+
+    /**
+     * @param $data
+     * @return mixed
+     * 通过id删除单个地址信息
+     */
+    public function deleteAddressById($id){
+
+        return DI()->notorm->member_address->where('id',$id)->delete();
+
+    }
+
+
 
     /**
      * 查看某会员是否有默认地址
