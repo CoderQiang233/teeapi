f201e16eec4d0584b8a7dca11516fb72ae3cc7d1
niuyuan <337966687@qq.com>
2018-12-12 17:14:42 +0800

    小程序订单页，订单详情页接口

---
+++ Weixin/Domain/ProductOrder.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lxl
+ * Date: 2018/7/17
+ * Time: 下午3:37
+ */
+class Domain_ProductOrder
+{
+
+    public function findProductOrder($data){
+
+        try{
+
+            $model=new Model_ProductOrder();
+
+            $res=$model->findProductOrder($data);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('查看客户订单信息失败','客户id:'.$data->member_id.'异常信息:'.$e);
+
+            return false;
+        }
+
+    }
+
+
+    public function findAllProductOrder($data){
+
+        try{
+
+            $model=new Model_ProductOrder();
+
+            $res=$model->findAllProductOrder($data);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过会员id查看全部会员订单信息失败','客户id:'.$data->member_id.'异常信息:'.$e);
+
+            return false;
+        }
+
+    }
+
+    public function findProductOrderById($data){
+
+        try{
+
+            $model=new Model_ProductOrder();
+
+            $res=$model->findProductOrderById($data->product_order_id);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过id查看订单信息失败','订单id:'.$data->product_order_id.'异常信息:'.$e);
+
+            return false;
+        }
+    }
+
+    public function deleteProductOrderById($data){
+
+        try{
+
+            $model=new Model_ProductOrder();
+
+            $res=$model->deleteProductOrderById($data->product_order_id);
+
+            return $res;
+
+        }catch (Exception $e){
+
+            DI()->logger->error('通过id删除订单信息失败','订单id:'.$data->product_order_id.'异常信息:'.$e);
+
+            return false;
+        }
+    }
+
+
+}
