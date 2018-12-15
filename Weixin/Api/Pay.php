fd1bf4ca6102024ceb96af1e0f89dd1ff68b5960
wangxin <1602595374@qq.com>
2018-12-15 14:36:47 +0800

    微信支付接通

---
+++ Weixin/Api/Pay.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * 支付API
+ *
+ * @author: dogstar <chanzonghuang@gmail.com> 2014-10-04
+ */
+class Api_Pay extends PhalApi_Api {
+
+	public function getRules() {
+        return array(
+
+            'addOrder' => array(
+                'product_name' => array('name' => 'product_name', 'require' => true,'type' => 'string','source' => 'post'),
+                'product_price' => array('name' => 'product_price', 'require' => true,'type' => 'string','source' => 'post'),
+                'product_num' => array('name' => 'product_num','require' => true,'type'=>'string','source' => 'post'),
+                'shipping_address'=>array('name' => 'shipping_address','require' => true,'type'=>'string','source' => 'post'),
+                'product_id'=>array('name' => 'product_id','require' => true,'type'=>'string','source' => 'post'),
+                'session3rd'=>array('name' => 'session3rd','require' => true,'type'=>'string','source' => 'post'),
+            ),
+
+        );
+	}
+
+
+
+
+
+
+
+	public function addOrder(){
+
+        $rs = array('code' => 0, 'msg' => '', 'info' => array());
+
+
+       // 获取对应的支付引擎
+        DI()->pay->set('wechat');
+
+        $session = DI()->wechatMini->getSession($this->session3rd);
+
+        DI()->logger->debug("session: ",array("session3rd"=>$session));
+
+        $data = array();
+
+        $data['order_id'] = DI()->pay->createOrderNo();
+
+        $data['product_name'] = $this -> product_name;
+
+        $data['product_price'] = $this -> product_price;
+
+        $data['product_num'] = $this -> product_num;
+
+        $data['openid'] = $session['openid'];
+
+        $data['pay'] = 0;
+
+      	$data['shipping_address'] = $this -> shipping_address;
+
+      	$data['ship_status'] = 0;
+
+        $data['product_id'] = $this -> product_id;
+
+        $data['status'] = 0;
+
+		$data['create_time'] = date('Y-m-d H:i:s',time());
+
+        $domain = new Domain_Pay();
+
+        $result = $domain->addOrder($data);
+
+        if($result){
+
+            $rs['code'] = 1;
+            $rs['msg'] ='会员订单成功并且获取统一订单成功';
+            $rs['info'] = $result;
+        }else{
+
+            $rs['msg'] ='生成会员订单失败';
+        }
+
+        return $rs;
+
+    }
+  
+
+
+
+
+}
