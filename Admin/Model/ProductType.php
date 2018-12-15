652885f08044eb5be03a76cd18e7cca429dcec5e
niuyuan <337966687@qq.com>
2018-12-15 16:18:25 +0800

    商品类别修改，删除

--- Admin/Model/ProductType.php
+++ Admin/Model/ProductType.php
@@ -21,159 +21,75 @@ class Model_ProductType extends PhalApi_Model_NotORM
         return $res;
     }
 
+    //获取全部选择框数据
+    public function getSelect(){
+
+        $res=DI()->notorm->product_type->select('product_type_id as value,name as label,parent_id,product_type_id')
+            ->fetchAll();
+
+        return $res;
+    }
+
+    public function  getSelectTwo(){
+
+        $sql= 'SELECT product_type_id as value,name as label,parent_id,product_type_id,type_parent  FROM shop_product_type WHERE parent_id IN '.
+             ' ( SELECT product_type_id FROM shop_product_type WHERE parent_id = 0) '.
+             'UNION SELECT product_type_id as value,name as label,parent_id,product_type_id,type_parent FROM shop_product_type WHERE parent_id = 0 ';
+
+        return DI()->notorm->product_type->queryAll($sql);
+    }
+
     //通过parent_id查询商品类别
     public function findTypeByParentId($parentId){
-        return DI()->notorm->product_type->where('parent_id',$parentId)->order('id')->fetchAll();
+        return DI()->notorm->product_type->where('parent_id',$parentId)->order('product_type_id')->fetchAll();
     }
 
-    public function insertProduct($data){
+    public function insertProductType($data){
 
-        try{
-            $info=array(
-                'name' => $data->name,
-                'first_picture' => $data->first_picture,
-                'detail' => $data->detail,
-                'create_time' => date("Y-m-d H:i:s"),
-                'brand' => $data->brand,
-                'intro' => $data->intro,
-                'market_price' => $data->market_price,
-                'agent_price' => $data->agent_price,
-                'num' => 0,//默认库存数为0
-            );
-            $result=DI()->notorm->commodity->insert($info);
+        $parentIds=json_decode($data->parent_id);
 
-            $banners=json_decode($data->banners);
-
-            if(count($banners)>0){
-                $rows=array();
-                for ($i=0;$i<count($banners);$i++){
-
-                    $rows[$i]['path'] = $banners[$i];
-
-                    $rows[$i]['is_use'] = '2';//是否使用(0未使用,1已使用)
-
-                    $rows[$i]['product_id'] = $result['id'];
-
-                    $rows[$i]['create_time'] = date("Y-m-d H:i:s");
-                }
-                $rs = DI()->notorm->banner->insert_multi($rows);
-            }
-
-            return true;
-
-        }catch (Exception $e){
-            return false;
+        if(count($parentIds)>0){
+            $parent_id=$parentIds[count($parentIds)-1];
         }
-    }
 
-
-    public function updateProduct($data){
-
-        try{
-
-            $info=array(
-                'name' => $data->name,
-                'first_picture' => $data->first_picture,
-                'detail' => $data->detail,
-                'brand' => $data->brand,
-                'intro' => $data->intro,
-                'market_price' => $data->market_price,
-                'agent_price' => $data->agent_price,
-            );
-            //Step 1: 开启事务
-            DI()->notorm->beginTransaction('db_daili');
-            //修改商品类别表
-            DI()->notorm->commodity->where('id', $data->id)->update($info);
-
-            //删除banner表中相关数据
-            DI()->notorm->banner->where('product_id', $data->id)->delete();
-
-            //插入banner表
-            $banners=json_decode($data->banners);
-
-            if(count($banners)>0){
-                $rows=array();
-                for ($i=0;$i<count($banners);$i++){
-
-                    $rows[$i]['path'] = $banners[$i];
-
-                    $rows[$i]['is_use'] = '2';//是否使用(0未使用,1已使用)
-
-                    $rows[$i]['product_id'] = $data->id;
-
-                    $rows[$i]['create_time'] = date("Y-m-d H:i:s");
-                }
-                $rs = DI()->notorm->banner->insert_multi($rows);
-            }
-
-            //Step 3: 提交事务
-            DI()->notorm->commit('db_daili');
-
-            return true;
-
-        }catch (Exception $e){
-
-            DI()->logger->log('updateProduct','商品类别修改失败',$e);
-
-            DI()->notorm->rollback('db_daili'); // 回滚
-
-            return false;
-        }
-    }
-
-    public function getById($id){
-
-        $result=DI()->notorm->commodity->where('id',$id) -> fetchOne();
-
-        $bannerAll=DI()->notorm->banner->where('product_id',$id) -> fetchAll();
-
-        $banners=array();
-
-        $imgUrl=DI()->config->get('app.imagePath');
-
-        for($i=0;$i<count($bannerAll);$i++){
-            $banners[$i]['uid']=$bannerAll[$i]['id'];
-            $banners[$i]['url']=$imgUrl.$bannerAll[$i]['path'];
-        }
-        $result['banners']=$banners;
+        $info=array(
+            'name' => $data->name,
+            'image_url' => $data->image_url,
+            'description' => $data->description,
+            'create_time' => date("Y-m-d H:i:s"),
+            'type_parent' => json_encode($parentIds),
+            'parent_id' => $parent_id,
+        );
+        $result=DI()->notorm->product_type->insert($info);
 
         return $result;
-
     }
+
+
+    public function updateProductType($data){
+
+        $parentIds=json_decode($data->parent_id);
+
+        if(count($parentIds)>0){
+            $parent_id=$parentIds[count($parentIds)-1];
+        }
+
+        $info=array(
+            'name' => $data->name,
+            'image_url' => $data->image_url,
+            'description' => $data->description,
+            'type_parent' => json_encode($parentIds),
+            'parent_id' => $parent_id,
+        );
+        $result=DI()->notorm->product_type->where('product_type_id',$data->product_type_id)->update($info);
+
+        return $result;
+    }
+
 
     public function deleteById($id){
 
-        try{
-            //删除
-            DI()->notorm->commodity->where('id', $id)->delete();
-
-            //删除
-            DI()->notorm->banner->where('product_id', $id)->delete();
-
-            return "success";
-
-        }catch (Exception $e){
-
-            DI()->logger->log('deleteProduct','删除商品类别id:'.$id,$e);
-
-            return "error";
-        }
-    }
-
-    public function getMemberLevelList(){
-
-        try{
-
-            return DI()->notorm->members_level->fetchAll();
-
-        }catch (Exception $e){
-
-            DI()->logger->log('findmemberlevel','查看会员等级',$e);
-
-            return false;
-        }
-
-
+        return DI()->notorm->product_type->where('product_type_id',$id)->delete();
     }
 
 
